1398782d04d930784f90ed87fc1ae80a3bcb818f
rahulbgautam <rahulbgautam@gmail.com>
2019-02-16 12:03:50 -0600

    Injected CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import numeral from "numeral"
+import './basic.css';
+import numeral from "numeral";
 
 
 let coursevalue = numeral(100000).format('$0,0.00');
-
+debugger;
 console.log((`Course value is ${coursevalue}`));
