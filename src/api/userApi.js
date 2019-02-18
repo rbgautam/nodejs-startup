18ec70dd2f65089892326c99eaa1831ee58f4b46
rahulbgautam <rahulbgautam@gmail.com>
2019-02-18 15:15:30 -0600

    added delete  functionality

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,6 +7,17 @@ export function getUsers(){
     return get('users');
 }
 
+export function deleteUser(id){
+    console.log(`deleting user id : ${id}`);
+    return delete(`users/${id}`);
+}
+
+const del = (url) => {
+    const request =  new Request(baseUrl+url,{ method:'DELETE'});
+    return fetch(request).then(onSuccess, onError);
+};
+
+
 function get(url){
     return fetch(baseUrl + url).then(onSuccess, onError);
 }    
