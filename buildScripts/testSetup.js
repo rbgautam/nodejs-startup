a2150df8d06495178761ace5596c37339d1eeb9d
Rahul Gautam <RGautam@iaai.com>
2019-03-14 08:49:28 -0500

    Added initial test

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+//This file isn't transpiled so must use CommonJS and ES5
+//Register babel to transpile before tests run
+require('babel-register')();
+
+//Disable webpack features that Mocha doesn't underestand
+require.extensions['.css'] = function(){};
